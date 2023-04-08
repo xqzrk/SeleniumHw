ef9757db8cb4f1b442f9725fe6319d36199386a0
xqzrk <120913909+xqzrk@users.noreply.github.com>
2023-04-08 10:11:26 +0400

    Update OrderTest.java

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -15,7 +16,7 @@ public class OrderTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
